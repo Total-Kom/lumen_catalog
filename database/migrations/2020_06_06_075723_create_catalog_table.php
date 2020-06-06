5cf817dcb4497ac4e5c385ff178eccb69adf29d3
TotalKom <totalcom4@gmail.com>
2020-06-06 11:12:22 +0300

    Added migration: files, category, catalog

---
+++ database/migrations/2020_06_06_075723_create_catalog_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatalogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalog', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user');
+            $table->bigInteger('id_file');
+            $table->bigInteger('id_category');
+            $table->unsignedInteger('rating')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalog');
+    }
+}
